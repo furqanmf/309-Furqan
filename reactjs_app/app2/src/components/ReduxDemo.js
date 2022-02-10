c0f88a80e285d0fc66e7275ecc9d404b8c42b633
furqanmf <fmf2497@gmail.com>
2022-02-10 22:19:11 +0530

    redux

--- reactjs_app/app2/src/components/ReduxDemo.js
+++ reactjs_app/app2/src/components/ReduxDemo.js
@@ -1,35 +1,45 @@
 import { useState } from "react";
-import store from "./myStore";
-//import store from "./playerReducer";
+// import store from "./playerReducer";
+import store from "../store/myStore";
+import NavBar from "./NavBar";
 
-const ReduxDemo = () =>{
+const ReduxDemo = (props) => {
 
-    //var storeData = store.getState();
-    //console.log("initial data in the store");
-    //console.log(storeData);
+    // var storeData = store.getState();
+    // console.log("initial data in the store");
+    // console.log(storeData);
 
 
-    const addOnePlayer = () =>{
-        console.log("dispatching ADD_ONE action");
-        store.dispatch( {type:"ADD_ONE"} );
-        //console.log("Latest Store data is : ");
-        //console.log(store.getState());
+    const addOnePlayer = () => {
+        // console.log("dispatching ADD_ONE action");
+        store.dispatch({ type: "ADD_ONE" });
+        // console.log("Latest Store data is : ");
+        // console.log(store.getState());
 
     }
 
-    const addTwoPlayer = () =>{
-        store.dispatch( {type:"ADD_TWO"} );
-        //console.log("Latest Store data is : ");
-        //console.log(store.getState());
+    const addTwoPlayer = () => {
+        store.dispatch({ type: "ADD_TWO" });
+        // console.log("Latest Store data is : ");
+        // console.log(store.getState());
 
-        
+
     }
 
-    const RemoveLastPlayer = () =>{
-        store.dispatch({type:"REMOVE_LAST"});
-        //console.log("Latest Store data is : ");
-        //console.log(store.getState());
+    const RemoveLastPlayer = () => {
+        store.dispatch({ type: "REMOVE_LAST" });
+        // console.log("Latest Store data is : ");
+        // console.log(store.getState());
 
+    }
+
+
+    const removeAge = (e) => {
+        e.preventDefault();
+        // console.log("dispatching remove by age");
+        store.dispatch({ type: "REMOVE_BY_AGE", playerAge: localState1.ageToRemove });
+        // console.log("latest store data is :");
+        // console.log(store.getState());
     }
 
 
@@ -47,9 +57,7 @@ const ReduxDemo = () =>{
         // console.log("latest store data is :");
         // console.log(store.getState());
     }
-    const removeAge = () => {
-        store.dispatch({ type: "REMOVE_AGE", playerAge: localState.ageToRemove });
-    }
+
 
     const [localState, setLocalState] = useState({ playerToRemove: "" });
 
@@ -73,27 +81,29 @@ const ReduxDemo = () =>{
         store.dispatch({ type: "ADD", playName: localState2.nAdd, playAge: localState2.aAdd, playAwards: localState2.awAdd })
     }
 
-
-    return(
+    return (
 
         <div>
+        <NavBar />
 
-        <h1>
-            !!!!!!!!!!!!Redux Demo!!!!!!!!!!!<br/>
-            Initial data from the store is:
-            <ul>
-                {store.getState().map((item,key) => <li key={key}>{item.name} {item.age} {item.awards.join(",")} </li>
-                    
-                )}
-            </ul>
-        </h1>
-        <button onClick={addOnePlayer}>Add one player</button><br/><br/>
-        <button onClick={addTwoPlayer}>Add two player</button><br/><br/>
-        <button onClick={RemoveLastPlayer}>Remove last player</button>
+            <h1>
+                !!!!!!!!!!!!Redux Demo!!!!!!!!!!!<br />
+                Initial data from the store is:
+                <ul>
+                    {store.getState().map((item, key) => <li key={key}>{item.name} {item.age} {item.awards.join(",")} </li>
+
+                    )}
+                </ul>
+            </h1>
+            <button onClick={addOnePlayer}>Add one player</button><br /><br />
+            <button onClick={addTwoPlayer}>Add two player</button><br /><br />
+            <button onClick={RemoveLastPlayer}>Remove last player</button><br /><br />
 
 
 
-        Enter Player Name : <input type="text" name="name" value={localState.playerToRemove} onChange={handleChange} /><br />
+
+
+            Enter Player Name : <input type="text" name="name" value={localState.playerToRemove} onChange={handleChange} /><br />
             <button onClick={removePlayer}>Remove the given player</button><br /><br />
 
             <form>REMOVE PLAYER BY AGE DEMO<br />
@@ -111,7 +121,14 @@ const ReduxDemo = () =>{
 
 
 
+
+
+
+
+
+
         </div>
+
 
     )
 }
